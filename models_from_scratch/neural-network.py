ab972bdb4cf9c62bc2ead390dfb57defd9640528
Muhammad Usman Shakeel <ushakeel808@gmail.com>
2021-08-07 12:38:54 +0500

    neural-network

---
+++ models_from_scratch/neural-network.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cvxopt
+from extras import create_dataset , plot_contour
+
+
+class NeuralNetwork():
+    def __init__(self,X,y):
+        pass 
+    def init_kaining_weights(self,10,11):
+        pass 
+    
+    def forward_prop(self,X,parameters):
+        pass 
+    
+    def compute_cost(self,y,probs,arameters):
+        pass 
+    
+    def back_prop(self,cache, parameters,y):
+        pass 
+    
+
+    def update_parameters(self, parameters, grads):
+        pass 
+
+    def main(self,X,y,num_iter = 10000):
+        pass 
